5bdb3555c70c88b527260c81c721796125ee3e17
Oleh <oleh.tsarevych@gmail.com>
2023-10-01 21:02:58 +0300

    Done

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import userData from './data/user.json';
+import { Profile } from './components/Profile/Profile';
+
+import stats from './data/statistics.json';
+import { Statistics } from './components/Statistics/Statistics';
+
+import friends from './data/friends.json';
+import { FriendList } from './components/Friends/FriendList';
+
+import transactions from './data/transactions.json';
+import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
+
+import styles from './App.module.css';
+
+export const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <Statistics title="Upload stats" stats={stats} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
